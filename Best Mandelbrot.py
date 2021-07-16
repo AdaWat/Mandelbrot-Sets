35af6be473baf1d41d95426bd27d9d5ac0dc95d0
Adam Watney <79660281+AdaWat@users.noreply.github.com>
2021-07-16 18:44:14 +0100

    Update Best Mandelbrot.py

--- Best Mandelbrot.py
+++ Best Mandelbrot.py
@@ -5,7 +5,7 @@ from math import log
 
 width = 875
 height = 500
-img = Image.new(mode='RGB', size=(width, height))       # ratio is 3.5:2
+img = Image.new(mode='RGB', size=(width, height))       # ratio is 7:4
 draw = ImageDraw.Draw(img)
 pc_size = (560, 320)
 
@@ -37,16 +37,10 @@ def mandelbrot(c):
 
 for x in range(width):
     for y in range(height):
-        #val = 1
         a = translate(x, 0, width, -2.5, 1)
         b = translate(y, 0, height, -1, 1)
         val = 100
         c = complex(a, b)
-
-        # hue = mandelbrot(c)
-        # rgb = hsv_to_rgb(hue, 1, val)
-        # rgb_real = (translate(rgb[0], 0, 1, 0, 255), translate(rgb[1], 0, 1, 0, 255), translate(rgb[2], 0, 1, 0, 255))
-        # px[x, y] = rgb_real
 
         hue = int(mandelbrot(c) * 255)
         draw.point([x, y], ImageColor.getrgb("hsv({}, 100%, {}%)".format(hue, val)))
